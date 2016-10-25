ad1b673ad1ef763d304e9520f1a425156cd9817f
andandandand <antonio.rueda.toicen@gmail.com>
2016-10-24 20:21:49 -0400

    stringBDM update for NAs

--- BDM-utils.R
+++ BDM-utils.R
@@ -4,6 +4,7 @@ library(acss)
 
 
 maxKs <- read.csv("./maxKs.csv")
+maxKs$X <- NULL
 
 countSymbols <- function(string){
   return(length(table(strsplit(string, NULL))))
@@ -64,23 +65,33 @@ splitString <- function(string, blockSize, offset){
   return (subs)
 }
 
+#receives the already splitted vector of input strings
 stringBDM <- function (stringsVector, base) {
   
-  #tally blocks
-  stringCounts <- table(stringsVector)
-  
-  counts <- as.data.frame(stringCounts)[,2]
+  stringCounts <- as.data.frame(table(stringsVector))
   
   #complexities
-  ks     <- acss(names(stringCounts), base)[,1]
   
-  bdm <- sum(log2(counts) + ks)
+  stringCounts["ks"] <- acss(as.vector(stringCounts[["stringsVector"]]), base)[, 1]
   
-  return(bdm)  
+  naIndices <- as.integer(which(is.na(ks)))
+  
+  naStrings <- as.vector(stringCounts$stringsVector[naIndices])
+  
+  naLengths <- unlist(lapply(naStrings, nchar))
+  
+  naKs <- maxKs[naLengths, paste0("K.", toString(base))]
+  
+  stringCounts[is.na(stringCounts)] <- naKs
+  
+  bdm <- sum(log2(stringCounts$Freq)) + sum(stringCounts$ks)
+  
+  return(bdm) 
+  
 }
 
+## should print 85.535
+#test <- stringBDM(c("000110100111","111001011000", "0110"),2)
+#test
 
-
-#test --should return NA
-stringBDM(c("000110100111","111001011000", "0110"),2)
 
